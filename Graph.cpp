215f63c5306146d256ebeec771d5089da9bdb701
John Steele <programjsteele@gmail.com>
2010-10-25 12:07:56 -0700

    Added method signitures and documentation from Graph.h

---
+++ Graph.cpp
@@ -0,0 +1,175 @@
+//====================================================================
+// Graph.h 
+//  
+// This class represents a Graph that is implemented using an 
+// adjacency list. A graph can not have more than 100 edges. 
+// The entire graph can be displayed or a specific path can be 
+// displayed. 
+// 
+// Author: John Steele <steelejr@u.washington.edu> 
+// Date  : 2010/11/02
+// Class : CSS 343 - Lab 3
+//====================================================================
+// Graph class: 
+//   Includes following features:
+// 	- allows building a graph. 
+//	- allows inserting an edge into the graph.
+//	- allows removing  an edge from the graph.
+//	- allows finding the shortest (lowest cost) path. 
+//	- allows displaying the graph.
+//	- allows dislplaying the graph from one vertex to another. 
+// Assumptions:
+// 	- input is properly formatted. 
+//	- description of each vertex is no longer than 50 characters.
+//	- each line following the description contains three ints
+//	  representing an edge. 
+//	- a zero for the first integer signals the end of the data. 
+//====================================================================
+
+#include "Graph.h"
+
+
+//========================Default-Constructor=========================
+// Creates a Graph object with default values. 
+// 
+// Preconditions: None.
+//		
+// Postconditions: this Graph object is instantiated with 
+//    		   default values. 
+//====================================================================
+Graph::Graph ()
+{
+
+}
+
+
+//========================Copy-Constructor============================
+// Creates a Graph object with the specified values set to the values
+// of the given Graph object.
+// 
+// Preconditions: the_other Graph object is not null, and has all 
+//		  correct invariance of a Graph object.
+//		
+// Postconditions: this Graph object is instantiated with its data
+//		   members set to the_other's. 
+//====================================================================
+Graph::Graph (const Graph &the_other)
+{
+
+}
+
+
+//========================Destructor==================================
+// Deallocates all resources obtained by this graph.
+// 
+// Preconditions: None.
+//		
+// Postconditions: All resources have been released. 
+//====================================================================
+Graph::~Graph ()
+{
+
+}
+
+
+//========================buildGraph==================================
+// Builds this graph using the provided ifstream. 
+//
+// The format of the stream should be as follows: 
+//	- The first line is the number of vertices.  
+//	- Flollowing is a text description of each vertex.  
+//	  (One description per line, and 50 chars max lenght)
+//	- After that, each line has 3 ints rep. an edge. 
+//	- A zero for the first int signals end of data. 
+// 
+// Preconditions: The input must be properly formatted, and be 
+//		  organize as described above. 
+//		
+// Postconditions: The graph is built using the data from the provided
+//		   stream.
+//====================================================================
+void Graph::buildGraph (ifstream &the_input)
+{
+
+}
+
+
+//========================insertEdge==================================
+// Inserts an edge into this graph. Inserting this edge must not
+// exceed the max of 100 edges. The first int is the from vertex, the
+// second is the to vertex, and the third int is the weight of the 
+// edge. 
+// 
+// Preconditions: The vertices must exist in the graph. Adding the
+//		  edge must not exceed the max edges of	100. 
+//		
+// Postconditions: Returns true if the edge was inserted into the
+//		   graph, false otherwise.
+//==================================================================== 
+bool Graph::insertEdge (int the_from_v, int the_to_v, int the_weight)
+{
+
+}
+
+
+//========================removeEdge==================================
+// Removes an edge from the graph. The first provided int is the from
+// vertex, and the second int is the to vertex. 
+// 
+// Preconditions: The edge must exist. The from vertex and to vertex
+//		  must exist.
+//		
+// Postconditions: Returns true if the edge was removed, returns false
+//		   otherwise.
+//==================================================================== 
+bool Graph::removeEdge (int the_from_v, int the_to_v) 
+{
+
+}
+
+
+//========================findShortestPath============================
+// Finds the shortest path between all vertices in the graph.  
+// 
+// Preconditions: The graph is built.
+//		
+// Postconditions: The shortest paths between all the vertices have
+//		   been found. 
+//==================================================================== 
+void Graph::findShortestPath ()
+{
+
+}
+
+
+//========================displayAll==================================
+// Displays the entire graph. Prints out the vertices description, the
+// edges associated with the vertices, and the edge weights. 
+// 
+// Preconditions: The graph must have already built. 
+//		
+// Postconditions: A detailed graph is desplayed showing all the 
+//		   vertices, the edges, and weights. 
+//==================================================================== 
+void Graph::displayAll ()
+{
+
+}
+
+
+//========================display=====================================
+// Displays the path from the first int to the second int. The ints
+// represents vertices in the graph, so they must exist. Prins out the
+// vertices description, the edges associated with the vertices, and
+// the edge weights. 
+// 
+// Preconditions: The provided vertices must exist.
+//		
+// Postconditions: A detailed graph is displayed showing all the 
+//		   vertices, their description, edges, and weights. 
+//==================================================================== 
+void Graph::display(int the_start_v, int the_finish_v)
+{
+
+} 
+
