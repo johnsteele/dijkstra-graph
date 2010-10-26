5e607efc462634ab11350d513be1c92302f03e9e
John Steele <programjsteele@gmail.com>
2010-10-26 08:39:01 -0700

    For some reason I originally saved the driver as a .c, so I renamed it with an extension of .cpp.

---
+++ driver.cpp
@@ -0,0 +1,48 @@
+//---------------------------------------------------------------------------
+// HW3.CPP
+// Driver code for testing the Graph class.
+// Authors:  Clark Olson and Carol Zander
+//---------------------------------------------------------------------------
+// This code tests the basic functionality of the class to
+// perform Dijkstra's algorithm for CSS 343 HW 3.
+// It is not meant to exhaustively test the class.
+//
+// Assumptions:
+//   -- a text file named "HW3.txt" exists in the same directory
+//      as the code and is formatted as described in the specifications
+//---------------------------------------------------------------------------
+#include <iostream>
+#include <fstream>
+#include "Graph.h"
+using namespace std;
+
+
+//-------------------------- main -------------------------------------------
+// Tests the Graph class by reading data from "HW3.txt" 
+// Preconditions:   If HW3.txt file exists, it must be formatted
+//                  as described in the specifications.
+// Postconditions:  The basic functionalities of the Graph class 
+//                  are used.  Should compile, run to completion, and output  
+//                  correct answers if the classes are implemented correctly.
+int main() {
+   ifstream infile1("HW3.txt");
+   if (!infile1) {
+      cerr << "File could not be opened." << endl;
+      return 1;
+   }
+
+   //for each graph, find the shortest path from every vertex to all other vertices
+   for(;;){
+      Graph G;
+      G.buildGraph(infile1);
+      if (infile1.eof()) 
+         break;
+      G.findShortestPath();        
+      G.displayAll();              // display shortest distance, path to cout
+      G.display(3,2);              // display path from vertex 3 to 2 to cout
+   }
+
+   return 0;
+}
+
+
